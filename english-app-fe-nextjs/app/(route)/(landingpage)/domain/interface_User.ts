4a9bac191b6cf90fe6ccccc09312c0bfd8a8d213
golangflutter <->
2024-08-08 09:41:49 +0700

    adding clean architecture example to project wwith anxios

---
+++ english-app-fe-nextjs/app/(route)/(landingpage)/domain/interface_User.ts
@@ -0,0 +1,6 @@
+// domain/interface_User.ts
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
---
+++ english-app-fe-nextjs/app/(route)/(landingpage)/presentation/UserList.tsx
@@ -0,0 +1,21 @@
+// components/UserList.tsx
+import React from 'react';
+import { useUsers } from '../controller/controller_user';
+
+
+const UserList: React.FC = () => {
+  const { users, loading, error } = useUsers();
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name} ({user.email})</li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
