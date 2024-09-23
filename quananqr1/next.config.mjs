6af3ccb944a67ba7326dd7c7459bbe1f451a5c93
golangflutter <->
2024-09-23 16:33:30 +0700

    start ok for public page

--- quananqr1/next.config.mjs
+++ quananqr1/next.config.mjs
@@ -1,4 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "images.pexels.com" },
+      { hostname: "images.unsplash.com" },
+      { hostname: "unsplash.com" },
+      { hostname: "example.com" },
+      { hostname: "127.0.0.1" },
+      { hostname: "localhost" },
+      { hostname: "shop-golang" }
+    ]
+  }
+};
 
 export default nextConfig;
