11ed94e3e07be0c61ad833b283c6cf6974d6658d
golangflutter <->
2024-09-27 15:23:08 +0700

    done for login and register

--- quananqr1/app/(public)/public-component/login-dialog.tsx
+++ quananqr1/app/(public)/public-component/login-dialog.tsx
@@ -1,37 +1,23 @@
 "use client";
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter, useSearchParams } from 'next/navigation';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger
-} from "@/components/ui/dialog";
-import {
-  Form,
-  FormField,
-  FormItem,
-  FormMessage
-} from "@/components/ui/form";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
-import { Info } from 'lucide-react';
 
-import { handleErrorApi } from '@/lib/utils';
-import { LoginBody, LoginBodyType } from '@/zusstand/auth/domain/auth.schema';
-import { useAuthStore } from '@/zusstand/auth/controller/auth-controller';
+import { Info } from "lucide-react";
+
+import { handleErrorApi } from "@/lib/utils";
+import { LoginBody, LoginBodyType } from "@/zusstand/auth/domain/auth.schema";
+import { useAuthStore } from "@/zusstand/auth/controller/auth-controller";
 
 const LoginDialog: React.FC = () => {
-  const { login, isLoginDialogOpen, openLoginDialog, closeLoginDialog } = useAuthStore();
+  const { login, isLoginDialogOpen, openLoginDialog, closeLoginDialog } =
+    useAuthStore();
   const searchParams = useSearchParams();
   const clearTokens = searchParams.get("clearTokens");
 
@@ -58,84 +44,85 @@ const LoginDialog: React.FC = () => {
   };
 
   return (
-    <Dialog open={isLoginDialogOpen} onOpenChange={(open) => open ? openLoginDialog() : closeLoginDialog()}>
+    <Dialog
+      open={isLoginDialogOpen}
+      onOpenChange={(open) => (open ? openLoginDialog() : closeLoginDialog())}
+    >
       <DialogTrigger asChild>
         <Button onClick={openLoginDialog}>Đăng nhập</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px] bg-white dark:bg-gray-800 shadow-lg">
-        <CardHeader>
-          <CardTitle className="text-2xl">Đăng nhập</CardTitle>
-          <CardDescription>
+        <DialogHeader>
+          <DialogTitle className="text-2xl">Đăng nhập</DialogTitle>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
             Nhập email và mật khẩu của bạn để đăng nhập vào hệ thống
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              className="space-y-4 w-full"
-              noValidate
-              onSubmit={form.handleSubmit(onSubmit, (err) => {
-                console.log(err);
-              })}
-            >
-              <div className="grid gap-4">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <div className="grid gap-2">
-                        <Label htmlFor="email">Email</Label>
-                        <Input
-                          id="email"
-                          type="email"
-                          placeholder="m@example.com"
-                          required
-                          className="border-2 border-gray-300  dark:border-gray-600 "
-                          {...field}
-                        />
-                        <FormMessage />
+          </p>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            className="space-y-4 w-full"
+            noValidate
+            onSubmit={form.handleSubmit(onSubmit, (err) => {
+              console.log(err);
+            })}
+          >
+            <div className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                        className="border-2 border-gray-300 dark:border-gray-600"
+                        {...field}
+                      />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-2">
+                      <Label htmlFor="password">Password</Label>
+                      <Input
+                        placeholder="password"
+                        id="password"
+                        type="password"
+                        required
+                        className="border-2 border-gray-300 dark:border-gray-600"
+                        {...field}
+                      />
+                      <div className="text-sm text-gray-500 dark:text-gray-400 flex items-center gap-1">
+                        <Info size={16} />
+                        <span>
+                          Password should be at least 8 characters long and
+                          include a mix of letters, numbers, and symbols.
+                        </span>
                       </div>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <div className="grid gap-2">
-                        <Label htmlFor="password">Password</Label>
-                        <Input
-                          placeholder="password"
-                          id="password"
-                          type="password"
-                          required
-                          className="border-2 border-gray-300  dark:border-gray-600"
-                          {...field}
-                        />
-                        <div className="text-sm text-gray-500 dark:text-gray-400 flex items-center gap-1">
-                          <Info size={16} />
-                          <span>
-                            Password should be at least 8 characters long and
-                            include a mix of letters, numbers, and symbols.
-                          </span>
-                        </div>
-                        <FormMessage />
-                      </div>
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="w-full">
-                  Đăng nhập
-                </Button>
-                <Button variant="outline" className="w-full" type="button">
-                  Đăng nhập bằng Google
-                </Button>
-              </div>
-            </form>
-          </Form>
-        </CardContent>
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Đăng nhập
+              </Button>
+              <Button variant="outline" className="w-full" type="button">
+                Đăng nhập bằng Google
+              </Button>
+            </div>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
--- quananqr1/app/(public)/public-component/register-dialog.tsx
+++ quananqr1/app/(public)/public-component/register-dialog.tsx
@@ -9,18 +9,14 @@ import {
   Dialog,
   DialogContent,
   DialogTrigger,
-  DialogTitle
+  DialogTitle,
+  DialogHeader
 } from "@/components/ui/dialog";
 import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardHeader,
-  CardDescription,
-  CardContent
-} from "@/components/ui/card";
+
 import { handleErrorApi } from "@/lib/utils";
 import { useAuthStore } from "@/zusstand/auth/controller/auth-controller";
 import {
@@ -107,25 +103,26 @@ const RegisterDialog = () => {
   //     "updated_at": "2024-08-19T16:17:16+07:00"
 
   // }
-  const onSubmit = async () => {
+  const onSubmit = async (data: RegisterBodyType) => {
     try {
       console.log(
-        "onSubmit register form quananqr1/app/(public)/public-component/register-dialog.tsx"
+        "onSubmit register form quananqr1/app/(public)/public-component/register-dialog.tsx data",
+        data
       );
 
       await register({
-        name: "Alice Jo1234f",
-        email: "alice.johnson@example.vvvvvvv",
-        password: "password123@%$@1234",
-        is_admin: false,
-        phone: 1234567890,
-        image: "alice.jpg",
-        address: "123 Main St, Anytown, USA",
-        created_at: "2024-08-19T16:17:16+07:00",
-        updated_at: "2024-08-19T16:17:16+07:00"
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        is_admin: data.is_admin,
+        phone: data.phone,
+        image: data.image,
+        address: data.address,
+        created_at: data.created_at,
+        updated_at: data.updated_at
       });
-      // setOpen(false);
-      // openLoginDialog();
+      setOpen(false);
+      openLoginDialog();
     } catch (error: any) {
       console.log("Error during registration: ", error);
       handleErrorApi({
@@ -138,147 +135,141 @@ const RegisterDialog = () => {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button>Đăng ký asdfasdf</Button>
+        <Button>Đăng ký</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px] bg-white dark:bg-gray-800 shadow-lg">
-        <DialogTitle className="text-2xl font-semibold">
-          Đăng ký sdFASDFASD
-        </DialogTitle>
-        <Card className="border-0 shadow-none">
-          <CardHeader>
-            <CardDescription>
-              Điền thông tin của bạn để tạo tài khoản mới
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Form {...form}>
-              <form
-                className="space-y-4 w-full"
-                noValidate
-                onSubmit={form.handleSubmit(onSubmit, (err) => {
-                  console.log(
-                    "Registration err onSubmit: quananqr1/app/(public)/public-component/register-dialog.tsx",
-                    err
-                  );
-                })}
-              >
-                <div className="grid gap-4">
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <div className="grid gap-2">
-                          <Label htmlFor="name">Họ và tên</Label>
-                          <Input
-                            id="name"
-                            type="text"
-                            placeholder="Alice Johnson"
-                            required
-                            className="border-2 border-gray-300 dark:border-gray-600"
-                            {...field}
-                          />
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <div className="grid gap-2">
-                          <Label htmlFor="email">Email</Label>
-                          <Input
-                            id="email"
-                            type="email"
-                            placeholder="alice.johnson@example.com"
-                            required
-                            className="border-2 border-gray-300 dark:border-gray-600"
-                            {...field}
-                          />
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <div className="grid gap-2">
-                          <Label htmlFor="password">Mật khẩu</Label>
-                          <Input
-                            id="password"
-                            type="password"
-                            placeholder="••••••••"
-                            required
-                            className="border-2 border-gray-300 dark:border-gray-600"
-                            {...field}
-                          />
-                          <div className="text-sm text-gray-500 dark:text-gray-400 flex items-center gap-1">
-                            <Info size={16} />
-                            <span>
-                              Mật khẩu phải có ít nhất 8 ký tự và bao gồm chữ
-                              cái, số và ký tự đặc biệt.
-                            </span>
-                          </div>
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="phone"
-                    render={({ field }) => (
-                      <FormItem>
-                        <div className="grid gap-2">
-                          <Label htmlFor="phone">Số điện thoại</Label>
-                          <Input
-                            id="phone"
-                            type="tel"
-                            placeholder="1234567890"
-                            required
-                            className="border-2 border-gray-300 dark:border-gray-600"
-                            {...field}
-                          />
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="address"
-                    render={({ field }) => (
-                      <FormItem>
-                        <div className="grid gap-2">
-                          <Label htmlFor="address">Địa chỉ</Label>
-                          <Input
-                            id="address"
-                            type="text"
-                            placeholder="123 Main St, Anytown, USA"
-                            required
-                            className="border-2 border-gray-300 dark:border-gray-600"
-                            {...field}
-                          />
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-                  <Button type="submit" className="w-full">
-                    Đăng ký
-                  </Button>
-                  <Button onClick={handleAddUser}>Add User</Button>
-                </div>
-              </form>
-            </Form>
-          </CardContent>
-        </Card>
+        <DialogHeader>
+          <DialogTitle className="text-2xl font-semibold">Đăng ký</DialogTitle>
+        </DialogHeader>
+        <p className="text-sm text-gray-500 dark:text-gray-400">
+          Điền thông tin của bạn để tạo tài khoản mới
+        </p>
+        <Form {...form}>
+          <form
+            className="space-y-4 w-full"
+            noValidate
+            onSubmit={form.handleSubmit(onSubmit, (err) => {
+              console.log(
+                "Registration err onSubmit: quananqr1/app/(public)/public-component/register-dialog.tsx",
+                err
+              );
+            })}
+          >
+            <div className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-2">
+                      <Label htmlFor="name">Họ và tên</Label>
+                      <Input
+                        id="name"
+                        type="text"
+                        placeholder="Alice Johnson"
+                        required
+                        className="border-2 border-gray-300 dark:border-gray-600"
+                        {...field}
+                      />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="alice.johnson@example.com"
+                        required
+                        className="border-2 border-gray-300 dark:border-gray-600"
+                        {...field}
+                      />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-2">
+                      <Label htmlFor="password">Mật khẩu</Label>
+                      <Input
+                        id="password"
+                        type="password"
+                        placeholder="••••••••"
+                        required
+                        className="border-2 border-gray-300 dark:border-gray-600"
+                        {...field}
+                      />
+                      <div className="text-sm text-gray-500 dark:text-gray-400 flex items-center gap-1">
+                        <Info size={16} />
+                        <span>
+                          Mật khẩu phải có ít nhất 8 ký tự và bao gồm chữ cái,
+                          số và ký tự đặc biệt.
+                        </span>
+                      </div>
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-2">
+                      <Label htmlFor="phone">Số điện thoại</Label>
+                      <Input
+                        id="phone"
+                        type="tel"
+                        placeholder="1234567890"
+                        required
+                        className="border-2 border-gray-300 dark:border-gray-600"
+                        {...field}
+                      />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-2">
+                      <Label htmlFor="address">Địa chỉ</Label>
+                      <Input
+                        id="address"
+                        type="text"
+                        placeholder="123 Main St, Anytown, USA"
+                        required
+                        className="border-2 border-gray-300 dark:border-gray-600"
+                        {...field}
+                      />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Đăng ký
+              </Button>
+              <Button onClick={handleAddUser}>Add User</Button>
+            </div>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
