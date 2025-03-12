1070d7c184039bed8c722f85c83d5dc1d93445db
golangflutter <->
2025-03-12 15:26:17 +0700

    asg

--- nextjs/app/(route)/user/data/datasources/userDataSource.ts
+++
@@ -1,8 +0,0 @@
-// src/domain/usecases/getUserUseCase.ts
-
-import { User } from "../../domain/entities/user";
-
-
-export interface GetUserUseCase {
-  execute(id: number): Promise<User>;
-}
--- nextjs/app/(route)/user/domain/usecase/getUserUseCase.ts
+++
@@ -1,6 +0,0 @@
-// src/domain/usecases/getUserUseCase.ts
-import { User } from '../entities/user';
-
-export interface GetUserUseCase {
-  execute(id: number): Promise<User>;
-}
